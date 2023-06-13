bfdeb155bfb59c17cbf3a8f505dc251d792be183
sushantkumar <sushantkumar@sushants-air>
2023-06-13 21:27:46 +0530

    FileSystem Done

--- com.lockedme/src/com/lockedme/FileSystem.java
+++ com.lockedme/src/com/lockedme/FileSystem.java
@@ -1,24 +1,59 @@
 package com.lockedme;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class FileSystem {
 	
+	private static String root = "/Users/sushantkumar/Desktop/lockedme/";
+	
+	public void setRoot(String path) {
+		File rootPath = new File(path);
+		
+		if(rootPath.exists()) {
+			FileSystem.root = path;
+		}
+	}
+		
+	
 	public static boolean createFile(String fileName) throws IOException{
-		File file = new File(fileName);
+		File file = new File(root+fileName);
 		return file.createNewFile();
 		
 	}
 	
 	public static String[] getFiles() {
-		File folder = new File("/");
+		File folder = new File(root);
 		return folder.list();		
 	}
 	
-	public static void deleteFile(String file) {
+	public static void deleteFile(String fileName) throws FileNotFoundException {
+		File file = new File(root+fileName);
 		
+		if(file.exists() == false)
+			throw new FileNotFoundException();
+		
+		file.delete();
+	}
+	
+	public static boolean searchFile(String fileName) {
+		File file = new File(root+fileName);
+		return file.exists();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			
+			FileSystem.createFile("somename.txt");
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		for(String s:FileSystem.getFiles())
+			System.out.println(s);
+	
 	}
 	
 }
